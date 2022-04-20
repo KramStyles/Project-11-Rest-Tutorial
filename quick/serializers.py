33c22474a967fcfe4f9e734b266e069fe7d8d2f8
kramstyles <mark.eke@decagon.dev>
2022-04-21 00:30:04 +0100

    added views

--- quick/serializers.py
+++ quick/serializers.py
@@ -3,4 +3,12 @@ from rest_framework import serializers
 
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
-    pass
+    class Meta:
+        model = User
+        fields = ['url', 'username', 'email', 'groups']
+
+
+class GroupSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Group
+        fields = ['url', 'name']
--- quick/views.py
+++ quick/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User, Group
+from rest_framework import viewsets, permissions
+from . import serializers as S
 
-# Create your views here.
+class UserViewSet(viewsets.ModelViewSet):
+    """Api endpoint that allows user to be viewed or edited"""
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = S.UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+class GroupViewSet(viewsets.ModelViewSet):
+    """Api endpoint that allows groups to be viewed or edited"""
+    queryset = Group.objects.all()
+    serializer_class = S.GroupSerializer
+    permission_classes = [permissions.IsAuthenticated]
