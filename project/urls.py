29afe298ec4931ee1322c68296feec71c848342b
kramstyles <mark.eke@decagon.dev>
2022-04-24 17:18:49 +0100

    added serializers and urls

--- project/urls.py
+++ project/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('quick.urls')),
     path('api/', include('snippets.urls')),
+    path('api/', include('posts.urls')),
 ]
